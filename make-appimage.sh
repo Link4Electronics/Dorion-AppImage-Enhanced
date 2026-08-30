2711439a5f84b3d7b46873cd0e300796bcc5aa49
Link4Electronics <link4electronics@gmail.com>
2026-08-30 06:28:45 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,8 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/512x512/apps/Dorion.png
 export DESKTOP=/usr/share/applications/Dorion.desktop
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/Dorion /usr/lib/Dorion
@@ -21,4 +20,4 @@ quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
