db4c0477063ef2cc947155824eafc53baa45155e
Link4Electronics <link4electronics@gmail.com>
2026-08-30 06:59:55 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,6 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/512x512/apps/Dorion.png
 export DESKTOP=/usr/share/applications/Dorion.desktop
-export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/Dorion /usr/lib/Dorion
